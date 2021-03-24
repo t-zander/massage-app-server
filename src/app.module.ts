f7cb1ba9f2b20b805be9fff2f7a073ddcb03f698
t-zander <alex.sergeevich.tarasenko@gmail.com>
2021-03-24 18:59:48 +0200

    finished with db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 @Module({
   imports: [
-    ConfigModule.forRoot(), // get variables from .env
-    MongooseModule.forRoot('mongodb://localhost/massage-app')
+    ConfigModule.forRoot({
+      isGlobal: true
+    }), // get variables from .env
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: Number(process.env.DB_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService]
 })
-export class AppModule {}
+export class AppModule { }
